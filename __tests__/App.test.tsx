b32d54f7394a5f09114f81d09e4e58336c2dab49
ctoverton <christianoverton@ctoverton.com>
2023-10-18 11:27:17 -0600

    added jest tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,12 +6,67 @@ import 'react-native';
 import React from 'react';
 import App from '@/App';
 
-// Note: import explicitly to use the types shiped with jest.
 import {it} from '@jest/globals';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+import {HomeScreen} from '@/screens/home';
+import useMenuItems from '@/hooks/useMenuItems';
+import {Featured} from '@/components/featured';
+import {Pressable} from 'react-native';
+
+jest.mock('@/hooks/useMenuItems');
+
+const mockUseMenuItems = useMenuItems as jest.MockedFunction<
+  typeof useMenuItems
+>;
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+describe('<HomeScreen />', () => {
+  it('renders correctly', () => {
+    mockUseMenuItems.mockReturnValue({
+      featured: [],
+      categories: [],
+    });
+    let props: any;
+    renderer.create(<HomeScreen {...props} />);
+  });
+});
+
+describe('<Featured />', () => {
+  it('renders correctly', () => {
+    mockUseMenuItems.mockReturnValue({
+      featured: [],
+      categories: [],
+    });
+
+    renderer.create(<Featured onPress={jest.fn()} />);
+  });
+
+  it('calls onPress when a featured item is pressed', () => {
+    const onPressMock = jest.fn();
+    const mockData = [
+      {
+        id: '1',
+        title: 'Pizza',
+        description: 'Saucy',
+        image: 1,
+        price: 10,
+      },
+    ];
+    mockUseMenuItems.mockReturnValue({
+      featured: mockData,
+      categories: [],
+    });
+
+    const component = renderer.create(<Featured onPress={onPressMock} />);
+    const pressableElements = component.root.findAllByType(Pressable);
+
+    pressableElements[0].props.onPress();
+
+    expect(onPressMock).toHaveBeenCalledWith(mockData[0]);
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,24 @@
+const {defaults: tsjPreset} = require('ts-jest/presets');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  ...tsjPreset,
   preset: 'react-native',
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFiles: ['<rootDir>/jest/setup.js'],
 };
