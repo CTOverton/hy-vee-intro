adf84fb549e1a1c94cf83d3ab1e94f98ca630a46
ctoverton <christianoverton@ctoverton.com>
2023-10-18 00:20:05 -0600

    init font awesome icons

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -2,6 +2,8 @@ import {RootStackParamList} from '@/navigations/rootNavigator';
 import {StackScreenProps} from '@react-navigation/stack';
 import {Pressable, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCircleCheck} from '@fortawesome/free-solid-svg-icons/faCircleCheck';
 
 type HomeScreenProps = StackScreenProps<RootStackParamList, 'Home'>;
 
@@ -11,8 +13,10 @@ export const HomeScreen = ({navigation}: HomeScreenProps) => {
       <View className={'justify-center items-center flex-1'}>
         <Text>Menu</Text>
         <Pressable onPress={() => navigation.navigate('Modal')}>
-          <Text className={'text-blue-600 mt-2'}>Open modal</Text>
+          <Text className={'text-blue-600 my-2'}>Open modal</Text>
         </Pressable>
+
+        <FontAwesomeIcon icon={faCircleCheck} />
       </View>
     </SafeAreaView>
   );
