f3a5644979dcab603c3ee9c24120f9b7daecaddc
ctoverton <christianoverton@ctoverton.com>
2023-10-18 00:13:53 -0600

    init pizza assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+export {default as Pizza_BBQ} from '@/assets/images/pizza-bbq.png';
+export {default as Pizza_Denver_Special} from '@/assets/images/pizza-denver-special.png';
+export {default as Pizza_Pepperoni} from '@/assets/images/pizza-pepperoni.png';
+export {default as Pizza_Plain} from '@/assets/images/pizza-plain.png';
+export {default as Pizza_Veggie} from '@/assets/images/pizza-veggie.png';
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,19 +1,19 @@
-import {RootStackParamList} from "@/navigations/rootNavigator";
-import {StackScreenProps} from "@react-navigation/stack";
-import {Pressable, Text, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {RootStackParamList} from '@/navigations/rootNavigator';
+import {StackScreenProps} from '@react-navigation/stack';
+import {Pressable, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-type HomeScreenProps = StackScreenProps<RootStackParamList, 'Home'>
+type HomeScreenProps = StackScreenProps<RootStackParamList, 'Home'>;
 
 export const HomeScreen = ({navigation}: HomeScreenProps) => {
-    return (
-        <SafeAreaView className={'flex-1'}>
-            <View className={'justify-center items-center flex-1'}>
-                <Text>Menu</Text>
-                <Pressable onPress={() => navigation.navigate('Modal')}>
-                    <Text className={'text-blue-600 mt-2'}>Open modal</Text>
-                </Pressable>
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView className={'flex-1'}>
+      <View className={'justify-center items-center flex-1'}>
+        <Text>Menu</Text>
+        <Pressable onPress={() => navigation.navigate('Modal')}>
+          <Text className={'text-blue-600 mt-2'}>Open modal</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
